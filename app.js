90f640e346e159a93a522825ff9302354f6293a3
Danielle Arias <daniellearias321@gmail.com>
2020-05-01 21:35:55 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+  $("#day").html(moment().format("MMM DD, YYYY"));
+  $(".time").each(function () {
+    var currentHour = moment().startOf("hour");
+    var comparedHour = moment().hour($(this).attr("data-time")).startOf("hour");
+
+    // add local storage value
+    $(this)
+      .children("form")
+      .children("textarea")
+      .val(localStorage.getItem($(this).attr("data-time")));
+
+    if (currentHour > comparedHour) {
+      // make the background of the div gray
+      $(this).addClass("past");
+    } else if (currentHour.isSame(comparedHour, "hour")) {
+      // make bkgrnd red
+      $(this).addClass("present");
+    } else {
+      // green background
+      $(this).addClass("future");
+    }
+  });
+
+  $("form").submit(function (event) {
+    event.preventDefault();
+    var task = event.target.task.value;
+    var hour = $(this).parent().attr("data-time");
+
+    localStorage.setItem(hour, task);
+  });
+});
